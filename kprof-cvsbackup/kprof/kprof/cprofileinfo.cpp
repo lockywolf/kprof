ca540f0039d1c7b0c3f334e86012d5e13d893a9d
cdesmond <cdesmond@929e6eca-fa19-0410-be49-db8c836f6d4a>
2002-07-28 18:48:59 +0000

    Extended the Html generate code to put more information about callers and called methods in a table.

--- kprof-cvsbackup/kprof/kprof/cprofileinfo.cpp
+++ kprof-cvsbackup/kprof/kprof/cprofileinfo.cpp
@@ -38,12 +38,16 @@ CProfileInfo::~CProfileInfo()
 {
 }
 
+//Dump information relevant to the class into a HTML file
+//for viewing in one of the tabbed panes.
 void CProfileInfo::dumpHtml()
 {
+	//Open the temporary file used to store the HTML.
 	QFile dumpFile;
 	QString fileName = "/tmp/" + htmlName + "::" + method + ".html" ;
 	dumpFile.setName(fileName);
 
+	//If there is already a file of the same name, delete it.
 	if (dumpFile.exists())
 	{
 		dumpFile.remove();
@@ -53,9 +57,19 @@ void CProfileInfo::dumpHtml()
 
 	QByteArray dumpText;
 	QTextOStream stream (dumpText);
-
+	
+    //Start the HTML code
 	stream << "<HTML><BODY>" << endl;
-	stream << "<H1>" << htmlName << "::" << method << "</H1>" << endl;
+	
+	//Put a heading on the page, check for C style functions with no method.
+	if (method != "")
+	{
+		stream << "<H1>" << htmlName << "::" << method << "</H1>" << endl;
+	}
+	else
+	{
+		stream << "<H1>" << htmlName << endl;
+	}
 	stream << "<P>" <<endl;
 	stream << "Arguments\t" << arguments << endl;
 	stream << "</P>" << endl;
@@ -73,29 +87,78 @@ void CProfileInfo::dumpHtml()
 	stream << "<P>Average time per call (ms)\t" << totalMsPerCall << "</P>" << endl;
 	stream << "<P>Cumulative percentage\t" << cumPercent << "</P>" << endl;
 	stream << "<H2>Callers</H2>" << endl;
+
+	if(callers.count())
+	{
+		stream << "<table>" << endl;
+		stream << "<TD>Name</TD>";
+		stream << "<TD>Cumulative Percent</TD>";
+		stream << "<TD>Cumulative Seconds</TD>";
+		stream << "<TD>Self Seconds</TD>";
+		stream << endl;
+	}
+
+	//Iterate through all the callers of the method
 	if (callers.count ())
 	{
 		for (uint i = 0; i < callers.count (); i++)
 		{
+			stream << "<TR>";
 			CProfileInfo *p = callers[i];
-			stream << "<LI><A HREF=\"/tmp/"<< p->htmlName << "::" << p->method << ".html\"> "
-						<< p->htmlName << "::" << p->method <<"</A></LI>" << endl;
+			if(p->method == "")
+			{
+				stream << "<TD><A HREF=\"/tmp/"<< p->htmlName << "::" << p->method << ".html\"> "
+							<< p->htmlName <<"</A></TD>" << endl;
+			}
+			else
+			{
+				stream << "<TD><A HREF=\"/tmp/"<< p->htmlName << "::" << p->method << ".html\"> "
+							<< p->htmlName << "::" << p->method <<"</A></TD>" << endl;
+			}
+			stream << "<TD>" << p->cumPercent << "</TD>" ;
+			stream << "<TD>" << p->cumSeconds << "</TD>";
+			stream << "<TD>" << p->selfSeconds;
+			stream << "</TR>" << endl;
   		}
    	}
+	stream << "</table>" << endl;
+
+	stream << "<table>" << endl;
+	
+	if (called.count())
+	{
+		stream << "<TD>Name</TD>";
+		stream << "<TD>Cumulative Percent</TD>";
+		stream << "<TD>Cumulative Seconds</TD>";
+		stream << "<TD>Self Seconds</TD>";
+		stream << endl;
+	}
+
+	//Iterator through all  the method called by this one.
 	stream << "<H2>Called</H2>" << endl;
-	if (callers.count ())
+	if (called.count ())
 	{
 		for (uint i = 0; i < called.count (); i++)
 		{
+			stream << "<TR>";
 			CProfileInfo *p = called[i];
-//			QString objectName = p->object;
-//			objectName.replace(QRegExp("<"), "[");
-//			objectName.replace(QRegExp(">"),"]");
-			stream << "<LI><A HREF=\"/tmp/"<< p->htmlName << "::" << p->method << ".html\"> "
-						<< p->htmlName << "::" << p->method <<"</A></LI>" << endl;
-   		}
-   	}
-	
+			if (p->method == "")
+			{
+				stream << "<TD><A HREF=\"/tmp/"<< p->htmlName << "::" << p->method << ".html\"> "
+							<< p->htmlName << "</A></TD>" << endl;
+			}
+			else
+			{
+				stream << "<TD><A HREF=\"/tmp/"<< p->htmlName << "::" << p->method << ".html\"> "
+							<< p->htmlName << "::" << p->method <<"</A></TD>" << endl;
+			}
+			stream << "<TD>" << p->cumPercent << "</TD>" ;
+			stream << "<TD>" << p->cumSeconds << "</TD>";
+			stream << "<TD>" << p->selfSeconds;
+			stream << "</TR>" << endl;
+		}
+	}
+	stream << "</table>" << endl;
 	
 	stream << "</BODY></HTML >" << endl;
 
